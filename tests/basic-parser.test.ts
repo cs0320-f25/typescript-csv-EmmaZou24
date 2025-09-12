ea1f33ac2d01c0073909ee932a58b9b90621933c
EmmaZou24 <emmabff7@gmail.com>
2025-09-12 00:51:36 -0400

    test: added tests for empty field csvs, different schemas, and refined schema

--- tests/basic-parser.test.ts
+++ tests/basic-parser.test.ts
@@ -8,6 +8,7 @@ const DOUBLEQUOTE_CSV_PATH = path.join(__dirname, "../data/doublequote.csv");
 const NUMBERS_CSV_PATH = path.join(__dirname, "../data/numbers.csv");
 const BASIC_PEOPLE_CSV_PATH = path.join(__dirname, "../data/basicpeople.csv");
 const ESCAPED_CSV_PATH = path.join(__dirname, "../data/escaped.csv");
+const EMPTYFIELDS_CSV_PATH = path.join(__dirname, "../data/emptyfields.csv");
 
 
 test("parseCSV yields arrays", async () => {
@@ -52,7 +53,27 @@ test("parseCSV obeys escaped quotes within fields", async () => {
   expect(results[2]).toEqual(["Smith", "Adam", "He chose \"CS 320\" at last"]);
 })
 
-test("parseCSV with a basic schema and CSV", async () => {
+test("parseCSV allows empty fields", async () => {
+  const results = await parseCSV(EMPTYFIELDS_CSV_PATH, undefined);
+  expect(results).toHaveLength(3);
+  expect(results[0]).toEqual(["apple", "20", "1.2", "minnesota"]);
+  expect(results[1]).toEqual(["pear", "13", "4.1", ""]);
+  expect(results[2]).toEqual(["orange", "", "5.8", "massachusetts"]);
+
+  //with a permissive schema that allows undefined fields
+  const schema = z.tuple([
+    z.string().optional(), z.coerce.number().optional(), z.coerce.number().optional(), z.string().optional()
+  ]).transform(tup => ({fruit: tup[0], num: tup[1], float: tup[2], state: tup[3]}));
+  const schema_results = await parseCSV(EMPTYFIELDS_CSV_PATH, schema);
+  expect(schema_results).toHaveLength(3);
+  expect(schema_results[0]).toEqual({fruit:"apple", num:20, float:1.2, state:"minnesota"});
+  expect(schema_results[1]).toEqual({fruit:"pear", num:13, float:4.1, state:undefined});
+  expect(schema_results[2]).toEqual({fruit:"orange", num:undefined, float:5.8, state:"massachusetts"});
+})
+
+/////////////////////////
+
+test("parseCSV with tuple schema", async () => {
   //Schema
   const schema = z.tuple([
     z.string(), //name
@@ -80,10 +101,30 @@ test("parseCSV with an improperly formatted CSV", async () => {
   
 })
 
-// test("parseCSV casts numbers as numbers", async () => {
-//   const results = await parseCSV(NUMBERS_CSV_PATH);
-//   expect(results).toHaveLength(3);
-//   expect(results[0]).toEqual(["red", "rose", "3"]);
-//   expect(results[1]).toEqual(["blue", "lily", "10"]);
-//   expect(results[2]).toEqual(["thirty", "grapefruit", "40"]);
-// })
+test("parseCSV with different schemas, same CSV", async () => {
+  const schema1 = z.tuple([
+    z.string().optional(), z.coerce.number().optional(), z.coerce.number().optional(), z.string().optional()
+  ]).transform(tup => ({fruit: tup[0], nums: [tup[1], tup[2]], state: tup[3]}));
+  const schema_results = await parseCSV(EMPTYFIELDS_CSV_PATH, schema1);
+  expect(schema_results).toHaveLength(3);
+  expect(schema_results[0]).toEqual({fruit:"apple", nums:[20, 1.2], state:"minnesota"});
+  expect(schema_results[1]).toEqual({fruit:"pear", nums:[13, 4.1], state:undefined});
+  expect(schema_results[2]).toEqual({fruit:"orange", nums:[undefined, 5.8], state:"massachusetts"});
+
+  const schema2 = z.tuple([
+    z.string().optional(), z.coerce.number().optional(), z.coerce.number().optional(), z.string().optional()
+  ]).transform(tup => ({fruit: tup[0], num: tup[1], float: tup[2], state: tup[3]}));
+  const schema_results2 = await parseCSV(EMPTYFIELDS_CSV_PATH, schema2);
+  expect(schema_results2).toHaveLength(3);
+  expect(schema_results2[0]).toEqual({fruit:"apple", num:20, float:1.2, state:"minnesota"});
+  expect(schema_results2[1]).toEqual({fruit:"pear", num:13, float:4.1, state:undefined});
+  expect(schema_results2[2]).toEqual({fruit:"orange", num:undefined, float:5.8, state:"massachusetts"});
+})
+
+test("parseCSV with refined schema", async () => {
+  const schema = z.tuple([
+    z.string(), //name
+    z.coerce.number().refine(num => num<50), //age less than 50
+  ])
+  await expect(parseCSV(BASIC_PEOPLE_CSV_PATH, schema)).rejects.toThrow();
+})
